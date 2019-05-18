e1966da0aaa175eb6f0d45bbc27940beb5586223
Javier de la Rosa <versae@gmail.com>
2019-05-18 16:46:43 +0200

    Updates

---
+++ precache-manifest.d6304f1d685be485cc75c798687c868c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639268620e1e566b265a45ffa1f4eddb",
+    "url": "/poetrylab/index.html"
+  },
+  {
+    "revision": "44d27f42110822f62ce6",
+    "url": "/poetrylab/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ff386c1a0233390b82dc",
+    "url": "/poetrylab/static/js/2.5c36e047.chunk.js"
+  },
+  {
+    "revision": "44d27f42110822f62ce6",
+    "url": "/poetrylab/static/js/main.53707d5c.chunk.js"
+  },
+  {
+    "revision": "35e141ab8d98d3975ca8",
+    "url": "/poetrylab/static/js/runtime~main.4ea33ea9.js"
+  }
+]);
