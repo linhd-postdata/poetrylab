21cbb2e6c99d24df2e303c3438c1d7f52e912ea5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-10 11:04:31 +0000

    Deploy linhd-postdata/poetrylab to github.com/linhd-postdata/poetrylab.git:gh-pages

---
+++ precache-manifest.95a1fc0821410ed2e739b7583a5888fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d824b6aa7d626993efa479dbd461587e",
+    "url": "/poetrylab/index.html"
+  },
+  {
+    "revision": "384f2799e78ae45e93a0",
+    "url": "/poetrylab/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cefe6aef407088a3be51",
+    "url": "/poetrylab/static/js/2.bd5057cb.chunk.js"
+  },
+  {
+    "revision": "384f2799e78ae45e93a0",
+    "url": "/poetrylab/static/js/main.6c75fe5f.chunk.js"
+  },
+  {
+    "revision": "35e141ab8d98d3975ca8",
+    "url": "/poetrylab/static/js/runtime~main.4ea33ea9.js"
+  }
+]);
